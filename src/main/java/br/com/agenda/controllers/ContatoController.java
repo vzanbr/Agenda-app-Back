098653d65fa05cf3b624ad1d1d28d9b0dc01a5d9
vzanbr <gabriecvr123@gmail.com>
2024-03-18 13:07:22 -0300

    Feat : Implementado metodo para inserir foto Get : foi inserido Requisições especifica para retorno do contato!

--- src/main/java/br/com/agenda/controllers/ContatoController.java
+++ src/main/java/br/com/agenda/controllers/ContatoController.java
@@ -3,10 +3,17 @@ package br.com.agenda.controllers;
 import br.com.agenda.model.entity.Contato;
 import br.com.agenda.model.repository.ContatoRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,9 +38,13 @@ public class ContatoController {
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public List<Contato> buscarTodos () {
-        return repository.findAll();
+    public Page<Contato> list(
+            @RequestParam(value = "page", defaultValue = "0")   Integer pagina,
+            @RequestParam(value = "size", defaultValue = "10")  Integer tamanhoPagina
+    ){
+        Sort sort = Sort.by(Sort.Direction.ASC, "nome");
+        PageRequest pageRequest = PageRequest.of(pagina, tamanhoPagina, sort);
+        return repository.findAll(pageRequest);
     }
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
@@ -42,11 +53,30 @@ public class ContatoController {
     }
 
     @PatchMapping("{id}/favorito")
-    public void favorite(@PathVariable Integer id, @RequestBody Boolean favorito) {
+    public void favorite( @PathVariable Integer id ){
         Optional<Contato> contato = repository.findById(id);
-        contato.ifPresent(c -> {
-            c.setFavorito(favorito);
+        contato.ifPresent( c -> {
+            boolean favorito = c.getFavorito() == Boolean.TRUE;
+            c.setFavorito(!favorito);
             repository.save(c);
         });
+    }
+    @PutMapping("{id}/foto")
+    public byte[] addPhoto(@PathVariable Integer id,
+                           @RequestParam("foto") Part arquivo){
+        Optional<Contato> contato = repository.findById(id);
+        return contato.map( c -> {
+            try{
+                InputStream is = arquivo.getInputStream();
+                byte[] bytes = new byte[(int) arquivo.getSize()];
+                IOUtils.readFully(is, bytes);
+                c.setFoto(bytes);
+                repository.save(c);
+                is.close();
+                return bytes;
+            }catch (IOException e){
+                return null;
+            }
+        }).orElse(null);
     }
 }
